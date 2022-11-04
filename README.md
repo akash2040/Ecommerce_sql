7e17517d21db22e7da37de89871793f464725b31
Akash Chanara <akashchanara@gmail.com>
2022-11-04 15:11:58 -0700

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
 # Ecommerce_sql
+
+# Description
+
+Javascript is used by programmers across the world to create dynamic and interactive web content like applications and browsers. JavaScript is so popular that it's the most used programming language in the world, used as a client-side programming language by 97.0% of all websites
+
+Internet retail, also known as e-commerce, is the largest sector of the electronics industry, having generated an estimated US$29 trillion in 2017 (Source: United Nations Conference on Trade and Development). E-commerce platforms like Shopify and WooCommerce provide a suite of services to businesses of all sizes. Due to the prevalence of these platforms, developers should understand the fundamental architecture of e-commerce sites.
+
+Walkthrough this Video[https://drive.google.com/file/d/1HNi_pxG28rjiiyMAwuDx1F-PvjGUwKVP/view]
+
+# Table of Content
+
+1. [HomePage](#homepage)
+2. [AccAcceptanceCriteria](#accAcceptancecriteria)
+3. [CodeSnippet](#codesnippet)
+4. [Tools](#tools)
+5. [Prerequisites](#prerequisites)
+6. [AuthorLinks](#authorlinks)
+
+# HomePage
+
+![pic](./img/)
+
+# AccAcceptanceCriteria
+
+- GIVEN a functional Express.js API
+- WHEN I add my database name, MySQL username, and MySQL password to an environment variable file
+- THEN I am able to connect to a database using Sequelize
+- WHEN I enter schema and seed commands
+- THEN a development database is created and is seeded with test data
+- WHEN I enter the command to invoke the application
+- THEN my server is started and the Sequelize models are synced to the MySQL database
+- WHEN I open API GET routes in Insomnia Core for categories, products, or tags
+- THEN the data for each of these routes is displayed in a formatted JSON
+- WHEN I test API POST, PUT, and DELETE routes in Insomnia Core
+- THEN I am able to successfully create, update, and delete data in my database
+
+# CodeSnippet
+
+```{
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+    },
+  },
+```
+
+```router.delete("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "no Category found with this id!" });
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+```
+
+# Tools
+
+1. VsCode
+2. Git (windows)
+3. Github
+4. MySQL2 package
+5. Dotenv package
+6. Sequelize
+
+# Prerequisites
+
+Software need to be install
+
+- Vscode
+- git
+- github(clone project)
+- npm install
+
+# AuthorLinks
+
+[Github](https://github.com/akash2040/Ecommerce_sql)
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,52 +1,35 @@
 const router = require("express").Router();
 const { Product, Category, Tag, ProductTag } = require("../../models");
 
-// The `/api/products` endpoint
-
 // get all products
 router.get("/", async (req, res) => {
   // find all products
-  try {
-    const productData = await Product.findAll({
-      include: [{ model: Category }, { model: Tag }],
-    });
-    res.status(200).json(productData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-  // be sure to include its associated Category and Tag data
+  // find all includes the category and tag
+  const productData = await Product.findAll({
+    include:
+      ({
+        model: Category,
+      },
+      { model: Tag }),
+  });
+  return res.json(productData);
 });
 
 // get one product
 router.get("/:id", async (req, res) => {
   // find a single product by its `id`
-  try {
-    const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category }, { model: Tag }],
-    });
-
-    if (!Product) {
-      res.status(404).json({ message: "No tag found with that id!" });
-      return;
-    }
-
-    res.status(200).json(productData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-  // be sure to include its associated Category and Tag data
+  const productData = await Product.findByPk(req.params.id, {
+    include:
+      ({
+        model: Category,
+      },
+      { model: Tag }),
+  });
+  return res.json(productData);
 });
 
-// create new product
-router.post("/", (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+// CREATE new product
+router.post("/", async (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -70,7 +53,7 @@ router.post("/", (req, res) => {
 });
 
 // update product
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -106,24 +89,18 @@ router.put("/:id", (req, res) => {
     })
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
-      // console.log(err);
       res.status(400).json(err);
     });
 });
 
 router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
-  try {
-    const productData = await Product.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
-
-    res.status(200).json(productData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  const productData = await Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  return res.json(productData);
 });
 
 module.exports = router;
